1986ec0ce45c220dc86701fcfad3428093377584
Mardin <mardinomer001@gmail.com>
2024-05-29 12:56:00 +0300

    react-hooks-event-handling-lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
     return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+      <div>
+        <input
+          type="password"
+          onChange={() => console.log("Entering password...")}
+        />
+      </div>
+    );
+  }
+  
+  export default Keypad;
